875ca4d515b9d6cca424e86932469b441da4cc37
Yomna-Raouf <yomnaraouf99@gmail.com>
2021-04-08 10:41:59 +0200

    consuming multiple contexts --ThemeContext & --AuthContext

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,33 +1,42 @@
 import "./Navbar.css";
 import { Component } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
+import { AuthContext } from "../../context/AuthContext";
 
 class Navbar extends Component {
   render() {
     return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <div className="navbar">
-              <nav
-                style={{
-                  background: theme.ui,
-                  color: theme.syntax,
-                }}
-              >
-                <h1>Context API</h1>
-                <ul>
-                  <li>Home</li>
-                  <li>About</li>
-                  <li> Contact</li>
-                </ul>
-              </nav>
-            </div>
-          );
-        }}
-      </ThemeContext.Consumer>
+      <AuthContext.Consumer>
+        {(authContext) => (
+          <ThemeContext.Consumer>
+            {(themeContext) => {
+              const { isLoggedIn, toggleLoggedIn } = authContext;
+              const { isLightTheme, light, dark } = themeContext;
+              const theme = isLightTheme ? light : dark;
+              return (
+                <div className="navbar">
+                  <nav
+                    style={{
+                      background: theme.ui,
+                      color: theme.syntax,
+                    }}
+                  >
+                    <h1>Context API</h1>
+                    <div onClick={toggleLoggedIn}>
+                      {isLoggedIn ? "logged in" : "logged out"}
+                    </div>
+                    <ul>
+                      <li>Home</li>
+                      <li>About</li>
+                      <li> Contact</li>
+                    </ul>
+                  </nav>
+                </div>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }

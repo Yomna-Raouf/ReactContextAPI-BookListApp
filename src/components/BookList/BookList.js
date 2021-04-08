9bdf1d32f15858ae447012773781f430dc92ba8c
Yomna-Raouf <yomnaraouf99@gmail.com>
2021-04-08 14:15:28 +0200

    migrated to functional components --consumed contexts using useContext hook

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,49 +1,46 @@
-import { Component } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 import "./BookList.css";
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
 
-    return (
-      <div
-        className="BookList"
-        style={{
-          background: theme.bg,
-        }}
-      >
-        <ul>
-          <li
-            style={{
-              background: theme.ui,
-              color: theme.syntax,
-            }}
-          >
-            the clean coder
-          </li>
-          <li
-            style={{
-              background: theme.ui,
-              color: theme.syntax,
-            }}
-          >
-            clean Code
-          </li>
-          <li
-            style={{
-              background: theme.ui,
-              color: theme.syntax,
-            }}
-          >
-            you don't know JavaScript yet
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      className="BookList"
+      style={{
+        background: theme.bg,
+      }}
+    >
+      <ul>
+        <li
+          style={{
+            background: theme.ui,
+            color: theme.syntax,
+          }}
+        >
+          the clean coder
+        </li>
+        <li
+          style={{
+            background: theme.ui,
+            color: theme.syntax,
+          }}
+        >
+          clean Code
+        </li>
+        <li
+          style={{
+            background: theme.ui,
+            color: theme.syntax,
+          }}
+        >
+          you don't know JavaScript yet
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export { BookList };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,44 +1,32 @@
 import "./Navbar.css";
-import { Component } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 import { AuthContext } from "../../context/AuthContext";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <AuthContext.Consumer>
-        {(authContext) => (
-          <ThemeContext.Consumer>
-            {(themeContext) => {
-              const { isLoggedIn, toggleLoggedIn } = authContext;
-              const { isLightTheme, light, dark } = themeContext;
-              const theme = isLightTheme ? light : dark;
-              return (
-                <div className="navbar">
-                  <nav
-                    style={{
-                      background: theme.ui,
-                      color: theme.syntax,
-                    }}
-                  >
-                    <h1>Context API</h1>
-                    <div onClick={toggleLoggedIn}>
-                      {isLoggedIn ? "logged in" : "logged out"}
-                    </div>
-                    <ul>
-                      <li>Home</li>
-                      <li>About</li>
-                      <li> Contact</li>
-                    </ul>
-                  </nav>
-                </div>
-              );
-            }}
-          </ThemeContext.Consumer>
-        )}
-      </AuthContext.Consumer>
-    );
-  }
-}
+const Navbar = () => {
+  const { isLoggedIn, toggleLoggedIn } = useContext(AuthContext);
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div className="navbar">
+      <nav
+        style={{
+          background: theme.ui,
+          color: theme.syntax,
+        }}
+      >
+        <h1>Context API</h1>
+        <div onClick={toggleLoggedIn}>
+          {isLoggedIn ? "logged in" : "logged out"}
+        </div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li> Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export { Navbar };

140e46fc9dd964a5aea88f9f06f2c50e6f77273f
Yomna-Raouf <yomnaraouf99@gmail.com>
2021-04-09 16:14:51 +0200

    added remove book action

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,23 +1,28 @@
 import { createContext, useState } from "react";
-
+import uuid from "uuid/v1";
 export const BookContext = createContext();
 
 const BookContextProvider = ({ children }) => {
   const [books, setBooks] = useState([
-    { title: "The clean coder", id: 1 },
-    { title: "clean code", id: 2 },
-    { title: "clean agile", id: 3 },
-    { title: "clean architecture", id: 4 },
-    { title: "you don't know javaScript yet", id: 5 },
+    { title: "The clean coder", author: "Robert C.Martin", id: 1 },
+    { title: "clean code", author: "Robert C.Martin", id: 2 },
+    { title: "clean agile", author: "Robert C.Martin", id: 3 },
+    { title: "clean architecture", author: "Robert C.Martin", id: 4 },
+    { title: "you don't know javaScript yet", author: "Kyle Simpson", id: 5 },
   ]);
 
-  const addBook = (e, newBook) => {
-    e.preventDefault();
-    setBooks({ ...books, newBook });
+  // ADD BOOK ACTION
+  const addBook = (newBook) => {
+    setBooks([...books, { ...newBook, id: uuid() }]);
+  };
+
+  // REMOVE BOOK ACTION
+  const removeBook = (id) => {
+    setBooks(books.filter((book) => book.id !== id));
   };
 
   return (
-    <BookContext.Provider value={{ books, addBook }}>
+    <BookContext.Provider value={{ books, addBook, removeBook }}>
       {children}
     </BookContext.Provider>
   );

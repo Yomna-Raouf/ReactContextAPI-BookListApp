d57ca5c0572fc03322215c2c349a79a5e8fe572b
Yomna-Raouf <yomnaraouf99@gmail.com>
2021-04-11 15:37:32 +0200

    moved actions to book reducer

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,28 +1,19 @@
-import { createContext, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { createContext, useReducer } from "react";
+import { bookReducer } from "../reducer/bookReducer";
+
 export const BookContext = createContext();
 
 const BookContextProvider = ({ children }) => {
-  const [books, setBooks] = useState([
-    { title: "The clean coder", author: "Robert C.Martin", id: 1 },
+  const [books, dispatch] = useReducer(bookReducer, []);
+
+  /*  { title: "The clean coder", author: "Robert C.Martin", id: 1 },
     { title: "clean code", author: "Robert C.Martin", id: 2 },
     { title: "clean agile", author: "Robert C.Martin", id: 3 },
     { title: "clean architecture", author: "Robert C.Martin", id: 4 },
-    { title: "you don't know javaScript yet", author: "Kyle Simpson", id: 5 },
-  ]);
-
-  // ADD BOOK ACTION
-  const addBook = (newBook) => {
-    setBooks([...books, { ...newBook, id: uuidv4() }]);
-  };
-
-  // REMOVE BOOK ACTION
-  const removeBook = (id) => {
-    setBooks(books.filter((book) => book.id !== id));
-  };
+    { title: "you don't know javaScript yet", author: "Kyle Simpson", id: 5 }, */
 
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {children}
     </BookContext.Provider>
   );

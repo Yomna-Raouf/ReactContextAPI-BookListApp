b8508e5a44600deb735545f97b824aa893072cf4
Yomna-Raouf <yomnaraouf99@gmail.com>
2021-04-11 10:55:47 +0200

    displayed number of books in users booklist --consumed BookContext

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,13 +2,16 @@ import "./Navbar.css";
 import { useContext } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 import { AuthContext } from "../../context/AuthContext";
+import { BookContext } from "../../context/BookContext";
 
 const Navbar = () => {
   const { isLoggedIn, toggleLoggedIn } = useContext(AuthContext);
   const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
   const theme = isLightTheme ? light : dark;
   return (
     <div className="navbar">
+      <p>Currently you have {books.length} books in your reading list</p>
       <nav
         style={{
           background: theme.ui,
